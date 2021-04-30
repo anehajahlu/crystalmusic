255c6c0b203777780d4beb9a156c582abd010d89
anehajahlu <80929752+anehajahlu@users.noreply.github.com>
2021-05-01 05:24:09 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -6,7 +6,7 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""<b>Haii Guys, Apa Kabar Kamu??:)) {message.from_user.first_name}!</b>
+        f"""<b>👋🏻 Haii Guys, Apa Kabar Kamu??:)) {message.from_user.first_name}!</b>
 
 Aku adalah Bot Musik Telegram, Apabila Ingin Menggunakan Aku Invite Aku Dan Assisten Aku Ya Biar Berjalan Dengan Lancar, Apabila Ada Kendala Tidak Tau Cara Pakainya Bisa PC OWNERNYA!:))
 ━━━━━━━━━━━━━━━━━━━━━━━━━
