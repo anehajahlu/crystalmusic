55b0477ae92a4ea541cc1409d9c88d4b12da3bda
anehajahlu <80929752+anehajahlu@users.noreply.github.com>
2021-05-01 04:30:47 +0700

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -23,7 +23,7 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search (masukan judul lagu)!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Mencari Lagu!....")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
