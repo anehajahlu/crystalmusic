57678f6cde2bae6528a0c9ca1d940137e179ab34
anehajahlu <80929752+anehajahlu@users.noreply.github.com>
2021-05-01 04:19:44 +0700

    Update inline.py

--- handlers/inline.py
+++ handlers/inline.py
@@ -13,7 +13,7 @@ async def inline(client: Client, query: InlineQuery):
         await client.answer_inline_query(
             query.id,
             results=answers,
-            switch_pm_text="Ketikkan nama Lagu/video di YouTube...",
+            switch_pm_text="Ketik Untuk Mencari Judul Lagu!...",
             switch_pm_parameter="help",
             cache_time=0
         )
