6cf5f69c5eb1bfeab73582a02cd19cceed5fc977
anehajahlu <80929752+anehajahlu@users.noreply.github.com>
2021-05-08 04:59:47 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -596,7 +596,7 @@ async def deezer(client: Client, message_: Message):
 
                           try:
                               await USER.join_chat(invitelink)
-                              await USER.send_message(message_.chat.id,"Saya bergabung dengan grup ini untuk memainkan musik di VCG")
+                              await USER.send_message(message_.chat.id,"Aku sudah bergabung disini, ayo kita musikan! 🖤")
                               await lel.edit(
                                   "<b>Assisten bot bergabung dengan obrolan Anda</b>",
                               )
